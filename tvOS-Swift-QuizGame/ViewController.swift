3473e256ab12115322e3a46f8b2b42409eb885dc
Pooya Hatami <pooya@node-1w7jr9qro06swfvrpgl5qlxd9.ipv6.telus.net>
2018-08-08 17:54:30 -0700

    done

--- tvOS-Swift-QuizGame/ViewController.swift
+++ tvOS-Swift-QuizGame/ViewController.swift
@@ -15,9 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func button0Tapped(sender: AnyObject) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    @IBAction func button1Tapped(sender: AnyObject) {
+        showAlert(status: "Correct!", title: "Whoo! That is the correct response")
+    }
+    @IBAction func button2Tapped(sender: AnyObject) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    @IBAction func button3Tapped(sender: AnyObject) {
+        showAlert(status: "Wrong!", title: "Bummer, you got it wrong!")
+    }
+    
+    func showAlert(status: String, title:String) {
+        let alertController = UIAlertController(title: status, message: title, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            
+        }
+        alertController.addAction(cancelAction)
+        
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+        }
+        alertController.addAction(ok)
+        
+        self.present(alertController, animated: true) {
+        }
     }
 
 
